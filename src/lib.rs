d5bf862595a7fb18a6a7392eec93dca234c71915
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-02-20 20:51:13 +0000

    Refactor main.rs by moving structs & enums to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use structopt::StructOpt;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Data {
+  pub primary: Vec<OptionValue>,
+  pub secondary: HashMap<String, Vec<OptionValue>>,
+  pub tertiary: HashMap<String, Vec<OptionValue>>
+}
+
+impl Data {
+  pub fn construct() -> Data {
+    let options_str = include_str!("options.json");
+    let v: Data = serde_json::from_str(options_str).unwrap();
+    v
+  }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum OptionValue {
+  TierOne {label: String, value: String},
+  TierTwo {label: String, value: String, usage: String},
+  TierThree {label: String, value: String, usage: String, nb: String}
+}
+
+#[derive(Debug, StructOpt)]
+/// Welcome to the Git Explore CLI,
+/// where you can search for git commands with natural language
+pub struct Cli {
+  /// Activate debug mode
+  // short and long flags (-d, --debug) will be deduced from the field's name
+  #[structopt(short, long)]
+  pub debug: bool,
+
+  /// Natural Language Search
+  pub search_terms: Vec<String>
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,13 @@
 use std::env;
 use std::process;
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use structopt::StructOpt;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Data {
-  pub primary: Vec<OptionValue>,
-  pub secondary: HashMap<String, Vec<OptionValue>>,
-  pub tertiary: HashMap<String, Vec<OptionValue>>
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(untagged)]
-enum OptionValue {
-  TierOne {label: String, value: String},
-  TierTwo {label: String, value: String, usage: String},
-  TierThree {label: String, value: String, usage: String, nb: String}
-}
+use git_explore::{Data, Cli};
 
 fn main() {
-  println!("Hello, world!");
-
-  let args: Vec<String> = env::args().collect();
-  println!("{:?}", args);
-
-  let options_str = include_str!("options.json");
-  println!("{:?}", options_str.len());
-
-  let v: Data = serde_json::from_str(options_str).unwrap();
-  println!("{:?}", v.primary);
+  let cli = Cli::from_args();
+  println!("{:?}", cli.search_terms);
+  
+  let d = Data::construct();
+  println!("{:?}", d.primary);
 }
