06626874ac2f497840057dee203de960bcbc2515
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-02-25 20:35:37 -0500

    Print name of program before running

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::process;
-use structopt::StructOpt;
 use ctrlc;
 use exitcode;
+use std::process;
+use structopt::StructOpt;
 
 use gitexplore::{Cli, Config};
 
@@ -10,12 +10,15 @@ fn main() {
     // Exit the program with 130 code to
     // signify ctrl-c
     process::exit(130);
-  }).expect("Error setting Ctrl-C handler");
-  
+  })
+  .expect("Error setting Ctrl-C handler");
+
   let config = Config::new(Cli::from_args()).unwrap_or_else(|err| {
     eprintln!("Problem parsing arguments: {}", err);
     process::exit(exitcode::USAGE);
   });
+
+  println!("Gitexplore CLI");
 
   if let Err(e) = gitexplore::run(config) {
     eprintln!("Application error: {}", e);
